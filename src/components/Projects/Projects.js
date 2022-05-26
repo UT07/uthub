34719dff2bd03fcd09f3a557453f6a96cc624c95
Utkarsh Singh <ut254_7@hotmail.com>
2022-05-26 03:57:10 -0500

    added new projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,8 +8,9 @@ import carz3330 from "../../Assets/Projects/carz3330.png";
 import yourmeals from "../../Assets/Projects/yourmeals.gif";
 import Cryptomania from "../../Assets/Projects/cryptomania.png";
 import demo from "../../Assets/Projects/Demo.gif";
-
-
+import statify from '../../Assets/Projects/statify.png';
+import shlack from '../../Assets/Projects/shlack.png';
+import utube from '../../Assets/Projects/ut-tube.png';
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -22,13 +23,49 @@ function Projects() {
           Here are a few applications I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={statify}
+              isBlog={false}
+              title="Statify"
+              description="Statify is a website that allows users to examine their Spotify account statistics, including Top Songs, Favorite Artists, Top Genres, Recommendations, and a visual depiction of all of the above in form of graphs and charts."
+              link="https://statify-tool.herokuapp.com/ "
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={carz3330}
               isBlog={false}
               title="Carz 3330"
               description="▪ Developed a full-stack web application that enables users to rent a car and manage bookings. Application also allows vendors to manage the car inventory and request maintenance on over 500 vehicles."
-              link="https://github.com/UT07/carzz"
+              link="https://carz3330.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Cryptomania}
+              isBlog={false}
+              title="Cryptomania"
+              description="Implemented a GitHub hosted data visualization cryptocurrency web application that retrieves data from real-time cryptocurrency pricing API and made a time-series analysis graph for the selected coins."
+              link="https://crypto-mania.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={utube}
+              isBlog={false}
+              title="UT-Tube"
+              description="Developing a live Github hosted Youtube downloader, using React, JavaScript, HTML5, CSS3, Youtube Data v3 API."
+              link="https://ut-tube.netlify.app"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={shlack}
+              isBlog={false}
+              title="Shlack"
+              description="Shlack is a agile communication application made with React, Redux and Firebase which sends and receives messages instantly to other users in the chat. It also shows when users are online and offline"
+              link="https://shlack-d0d44.web.app/"
             />
           </Col>
 
@@ -38,18 +75,10 @@ function Projects() {
               isBlog={false}
               title="Lightlib LMS"
               description="A lightweight library management system built in .NET 5.0 with Entity Framework Core. Enables library administrators to manage Library Assets, Patrons, and Branches with basic functionality. This project is intended to serve as a long-term resource for small .NET web monoliths."
+              link="https://github.com/UT07/lightlib-lms"
             />
-          </Col>
-          
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Cryptomania}
-              isBlog={false}
-              title="Cryptomania"
-              description="Implemented a GitHub hosted data visualization cryptocurrency web application that retreives data from real-time cryptocurrency pricing API and made a time-series analysis graph for the selected coins."
-              link="https://crypto-mania.netlify.app/"
-            />
-          </Col>
+          </Col> 
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={demo}
