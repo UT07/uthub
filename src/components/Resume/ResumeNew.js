733430570bbdaca3681a8a3535ff8d2abfe46c84
Utkarsh Singh <ut254_7@hotmail.com>
2022-08-11 02:43:19 -0500

    upadted the resume to indian resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Utkarsh_Singh_Resume.pdf";
+import pdf from "../../Assets/Utkarsh_Singh_IND.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/UT07/uthub/master/src/Assets/Utkarsh_Singh_Intern.pdf";
+  "https://raw.githubusercontent.com/UT07/uthub/master/src/Assets/Utkarsh_Singh_IND.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
